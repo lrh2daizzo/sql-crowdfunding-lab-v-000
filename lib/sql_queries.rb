00682c44fe4bd7570baec005924943e2a8e0ee2c
Luis Herrera <herrera.lrh@gmail.com>
2019-11-20 22:12:22 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -38,5 +38,5 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
   "SELECT projects.category, pledges.amount
    FROM projects
    JOIN pledges ON projects.id = pledges.project_id
-   GROUP BY projects.category HAVING projects.category = 'music';"
+   WHERE projects.category = 'music';"
  end
