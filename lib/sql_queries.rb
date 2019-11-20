b24fa17e777e9d2ee9c0f3e3ca2aa037c736b2b7
Luis Herrera <herrera.lrh@gmail.com>
2019-11-20 21:45:36 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -10,5 +10,5 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
   "SELECT projects.title, SUM(pledges.amount)
    FROM projects
    JOIN pledges ON projects.id = pledges.project_id
-   ORDER BY projects.title;"
+   GROUP BY projects.title;"
  end
